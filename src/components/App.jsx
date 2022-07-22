126e96a91b16a6b8b78b1326735969ab3ce3afa4
sergiy-smaga <seregasmaga@gmail.com>
2022-07-22 20:21:16 +0300

    1 task done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
+import css from './App.module.css';
+import { Profile } from './Profile/Profile';
+import user from '../user.json/user.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.App}>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
