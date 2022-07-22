ed4de69f555c8b88b761c581d014f371306aba84
sergiy-smaga <seregasmaga@gmail.com>
2022-07-22 21:04:21 +0300

    2nd task almost done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,18 @@
 import css from './App.module.css';
 import { Profile } from './Profile/Profile';
-import user from '../user.json/user.json';
+import { Statistics } from './Statistics/Statistics';
+import user from 'user.json';
+import data from 'data.json';
 
-export const App = () => {
-  return (
-    <div className={css.App}>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-    </div>
-  );
-};
+export const App = () => (
+  <div className={css.App}>
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+    <Statistics title="Upload stats" stats={data} />
+  </div>
+);
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
+
+export const Statistics = ({ title, stats }) => (
+  <section className={css.statistics}>
+    <h2 className={css.title}>{title}</h2>
+
+    <ul className={css.statList}>
+      {stats.map(stat => (
+        <li key={stat.id}>
+          <span className={css.label}>{stat.label}</span>
+          <span className={css.percentage}>{stat.percentage}</span>
+        </li>
+      ))}
+    </ul>
+  </section>
+);
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
+};
